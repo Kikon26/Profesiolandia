01d87cb3e4f0309f41348ef24400266bcfc1776d
Kikon26 <enrique.rocko@gmail.com>
2021-04-30 10:10:57 -0500

    Registro light

--- application/controllers/CRegistro.php
+++ application/controllers/CRegistro.php
@@ -13,10 +13,18 @@ class CRegistro extends CI_Controller {
 		$this->load->model('MMenu');
     }
     public function index(){
-		if (isset($this->session->gIdPerfil)) $tabla = $this->MMenu->MenuRol($this->session->gIdPerfil);
-		else 								  $tabla = $this->MMenu->MenuRol(4);		
+		if (isset($this->session->gIdPerfil)) 
+		 {
+			 $tabla = $this->MMenu->MenuRol($this->session->gIdPerfil);
+
+		 }
+		else 								  
+			$tabla = $this->MMenu->MenuRol(4);		
+
+
+
         $data = array(
-            'seccion' => 'registro',
+            'seccion' => 'Registro',
 			'vista' => 'VRegistro',
 			'data' => '',
 			'dataf' => '',
@@ -26,41 +34,13 @@ class CRegistro extends CI_Controller {
 		$this->load->view('mp/pagina_principal',$data);
 	}
 
-	public function save(){				
+	public function Usuario(){
+		$resultado['usuario'] = $this->MRegistro->DetalleUsuario($this->input->get('id_cat_usuario'));				
+		echo json_encode($resultado);
+	}
 
-		if ($this->input->post('id_cat_rol')==2)	$config['upload_path']="./assets/images/profesionales";              		             		                         	
-		else 										$config['upload_path']="./assets/images/users";              		             	
 	
-        $config['allowed_types']='gif|jpg|jpeg|png';
-		$config['encrypt_name'] = false;
-		$config['file_name'] =$this->input->post('usuario')."-".date("Y-m-d-H-i-s")."-".$_FILES['file']['name'];
-		         
-		$this->load->library('upload',$config);
 
-		if ( ! $this->upload->do_upload("file"))		
-		{
-				$error = array('error' => $this->upload->display_errors());                				
-				var_dump($error);				
-		}
-		else
-		{
-			$data = array('upload_data' => $this->upload->data());		
-			$file_name= $data['upload_data']['file_name']; 
-
-			$resultado['save']=$this->MRegistro->save_usuario($file_name);                
-			echo json_encode($resultado);
-		}
-    }
-  	
-	public function rol(){
-		$resultado['rol'] = $this->MRegistro->CatalogoRoles();				
-		echo json_encode($resultado);
-	}
-
-	public function estado(){
-		$resultado['estado'] = $this->MRegistro->CatalogoEstados();				
-		echo json_encode($resultado);
-	}
 }
 
 
--- application/models/MRegistro.php
+++ application/models/MRegistro.php
@@ -12,108 +12,38 @@ class MRegistro extends CI_Model {
 	{	
 	
     }
-    
-    public function save_usuario($file_name)
-    {
-      $sqlsrvDB = $this->load->database('dbProfesiolandia',TRUE);            
-      $postData = $this->input->post();
-      
-      if ($postData['id_cat_rol']==2)
-      {
-          $data = array(              
-            'usuario'  => $postData['usuario'], 
-            'password'  => sha1($postData['password']), 
-    
-            //'especialidad'  => $postData['especialidad'], 
-            //'descripcion'  => $postData['descripcion'], 
-    
-            'nombre'  => $postData['nombre'], 
-            'paterno'  => $postData['paterno'], 
-            'materno'  => $postData['materno'], 
-            'id_cat_rol'  => "2", 
-            'email'  => $postData['email'], 
 
-            //'id_cat_profesion'  => $postData['id_cat_profesion'],  
-            //'costo_consulta'  => $postData['costo_consulta'],  
+	public function DetalleUsuario($id_cat_usuario)
+  {
+    $sqlsrvDB = $this->load->database('dbProfesiolandia',TRUE);
+    $postData = $this->input->post();
 
-            'imagen'  => $file_name,                   
-            'fecha_alta' => date("Y-m-d H:i:s"),
-            'activo' => 1
-          );      
-    
-          $resultado=$sqlsrvDB->insert('cat_profesionales',$data);
-          $id_cat_profesional=$sqlsrvDB->insert_id();
-          /*************************************************************************/
-          $data = array(     
-            'id_cat_profesional'  => $id_cat_profesional,          
-            'id_cat_estado'  => $postData['id_cat_estado'], 
-            'municipio'  => $postData['mpio'], 
-            'colonia'  => $postData['colonia'], 
-            'calle'  => $postData['calle'], 
-            'num'  => $postData['num'], 
-            'cp'  => $postData['cp'],         
-            'tel'  => $postData['telefono'],          
-            'activo' => 1
-          );      
-          $resultado=$sqlsrvDB->insert('cat_direcciones',$data);
-      }
-      else 
-      {
-          $data = array(              
-            'usuario'  => $postData['usuario'], 
-            'password'  => sha1($postData['password']), 
-            'nombre'  => $postData['nombre'], 
-            'paterno'  => $postData['paterno'], 
-            'materno'  => $postData['materno'], 
-            'email'  => $postData['email'], 
-            'id_cat_rol'  => $postData['id_cat_rol'],  
-            'imagen'  => $file_name,                   
-            'fecha_alta' => date("Y-m-d H:i:s"),
-            'activo' => 1
-          );
-              
-          $resultado=$sqlsrvDB->insert('usuarios',$data);           
-          $id_cat_usuario=$sqlsrvDB->insert_id();
-          
-          $data = array(     
-            'id_cat_usuario'  => $id_cat_usuario,          
-            'id_cat_estado'  => $postData['id_cat_estado'], 
-            'municipio'  => $postData['mpio'], 
-            'colonia'  => $postData['colonia'], 
-            'calle'  => $postData['calle'], 
-            'num'  => $postData['num'], 
-            'cp'  => $postData['cp'],         
-            'tel'  => $postData['telefono'],          
-            'activo' => 1
-          );      
-          $resultado=$sqlsrvDB->insert('cat_direcciones',$data); 
-      }
-      
+    $query="select  
+            s.nombre as estado,                                    
+			p.usuario,
+            p.nombre,
+			p.paterno,
+			p.materno,
+			p.email,
+			p.imagen,
+            d.id_cat_estado,
+            d.municipio,
+            d.colonia,
+            d.calle,
+            d.num,
+            d.cp,
+            d.tel                                    
+            from usuarios as p inner join             
+            cat_direcciones as d on d.id_cat_usuario=p.id_cat_usuario and p.id_cat_usuario={$id_cat_usuario} left join 
+            cat_estados as s on s.id_cat_estado=d.id_cat_estado"; 
 
+  
+    $resultado = $sqlsrvDB->query($query);		
+    return $resultado->result();    
+  }  
 
-
-
-      return $resultado;              
-    } 
-
-
-    public function CatalogoRoles()
-    {
-		$sqlsrvDB = $this->load->database('dbProfesiolandia',TRUE);
-    $query="select id_cat_rol,descripcion from cat_roles where id_cat_rol in (2,3) order by id_cat_rol";         
-        $resultado = $sqlsrvDB->query($query);		
-		return $resultado->result();        
-    }
-
-
-    public function CatalogoEstados()
-    {
-		$sqlsrvDB = $this->load->database('dbProfesiolandia',TRUE);
-    $query="select * from cat_estados where activo=1 order by nombre";         
-        $resultado = $sqlsrvDB->query($query);		
-		return $resultado->result();        
-    }    
 	
+
 }
 
 ?>
