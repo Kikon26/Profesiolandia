56f6e0e66dfede13d48cc7a56787d4fdd48088ec
Martin Alberto Solis Trejo <martin.solis@morelia.gob.mx>
2021-05-28 16:08:31 -0500

    pestaña publicaciones

--- application/controllers/CAltaProfesional.php
+++ application/controllers/CAltaProfesional.php
@@ -7,10 +7,12 @@ class CAltaProfesional extends CI_Controller {
 		date_default_timezone_set('America/Mexico_City');			
 		$this->load->library('util');
 		$this->load->library('sesion');
+		$this->load->library("pagination");
 		$this->load->library('peticion');
 		
 		$this->load->model('MAltaProfesional');
 		$this->load->model('MMenu');
+
     }
     public function index(){
 		if (isset($this->session->gIdPerfil)) 
@@ -213,6 +215,78 @@ class CAltaProfesional extends CI_Controller {
 		echo json_encode($resultado);
 	}
 
+	public function loadRecord()
+	{
+		
+		$rowno=$this->input->post('pagno');
+
+		$config = array();
+        $config["base_url"] = base_url() . "CAltaProfesional";
+        $config["total_rows"] = $this->MAltaProfesional->get_count(); 
+        $config["per_page"] = 20;
+		$config["uri_segment"] = 2;		
+		
+		// custom paging configuration
+		$config['num_links'] = 4;
+		$config['use_page_numbers'] = TRUE;
+		$config['reuse_query_string'] = TRUE;
+		 
+		$config['full_tag_open'] = '<ul class="pagination">';
+		$config['full_tag_close'] = '</ul>';
+		 
+		$config['first_link'] = 'First Page';
+		$config['first_tag_open'] = '<span class="firstlink">';
+		$config['first_tag_close'] = '</span>';
+		 
+		$config['last_link'] = 'Last Page';
+		$config['last_tag_open'] = '<span class="lastlink">';
+		$config['last_tag_close'] = '</span>';
+		 
+		$config['next_link'] = 'Next Page';
+		$config['next_tag_open'] = '<li class="page-item">';
+		$config['next_tag_close'] = '</li>';
+
+		$config['prev_link'] = 'Prev Page';
+		$config['prev_tag_open'] = '<li class="page-item">';
+		$config['prev_tag_close'] = '</li>';
+
+		$config['cur_tag_open'] = '<li class="page-item">';
+		$config['cur_tag_close'] = '</li>';
+
+		$config['num_tag_open'] = '<li class="page-item">';
+		$config['num_tag_close'] = '</li>';
+		
+		$config['attributes'] = array('class' => 'page-link');
+        		
+		$this->pagination->initialize($config);
+
+		// Row position
+		if($rowno != 0){
+			$rowno = ($rowno-1) * $config["per_page"];			
+		}
+        
+		$page = $rowno;
+		$pagination_data["links"] = $this->pagination->create_links();
+		$pagination_data['publicaciones'] = $this->MAltaProfesional->ListadoPublicaciones($config["per_page"], $page);	
+		$pagination_data['row'] = $rowno;
+		
+		echo json_encode($pagination_data);
+	}	
+
+	public function save_update_publicacion(){
+		$resultado['save_update'] = $this->MAltaProfesional->save_update_publicacion();				
+		echo json_encode($resultado);
+	}
+
+	public function get_publicacion(){
+		$resultado['publicacion'] = $this->MAltaProfesional->get_publicacion();				
+		echo json_encode($resultado);
+	}
+
+	public function delete_publicacion(){
+		$resultado['delete'] = $this->MAltaProfesional->delete_publicacion();				
+		echo json_encode($resultado);
+	}
 }
 
 
---
+++ application/models/MProfesionalAdministraCuenta.php
@@ -0,0 +1,116 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MProfesionalAdministraCuenta extends CI_Model {
+
+	function __construct()
+	{
+    parent::__construct();
+    date_default_timezone_set('America/Mexico_City');
+	}
+
+	public function index()
+	{	
+	
+    }
+
+  public function DetalleUsuario($id_cat_profesional)
+  {
+    $sqlsrvDB = $this->load->database('dbProfesiolandia',TRUE);
+    $postData = $this->input->post();
+
+    $query="select  
+            s.nombre as estado,                                    
+            p.usuario,
+            p.nombre,
+            p.paterno,
+            p.materno,
+            p.email,
+            p.imagen,
+            d.id_cat_estado,
+            d.municipio,
+            d.colonia,
+            d.calle,
+            d.num,
+            d.cp,
+            d.tel                                    
+            from cat_profesionales as p left join             
+            cat_direcciones as d on d.id_cat_profesional=p.id_cat_profesional and d.dom_particular=1  left join 
+            cat_estados as s on s.id_cat_estado=d.id_cat_estado
+            where p.id_cat_profesional={$id_cat_profesional} "; 
+
+  
+    $resultado = $sqlsrvDB->query($query);		
+    return $resultado->result();    
+  }  
+
+  public function update_usuario($file_name)
+    {
+    $sqlsrvDB = $this->load->database('dbProfesiolandia',TRUE);            
+    $postData = $this->input->post();
+      
+    $data = array(              
+      'usuario'  => $postData['usuario'], 			
+      'nombre'  => $postData['nombre'], 
+      'paterno'  => $postData['paterno'], 
+      'materno'  => $postData['materno'], 
+      'email'  => $postData['email'], 					
+      'fecha_alta' => date("Y-m-d H:i:s"),
+      'activo' => 1
+      ); 
+      
+
+    if (isset($postData['imagen_perfil']))
+    {
+      $source = './assets/images/profesionales/'.$postData['imagen_perfil'];                 
+      unlink($source);       
+      $data['imagen'] = $file_name;
+    }		
+
+    if ( $postData['password']<>"")  
+      $data['password']= sha1($postData['password']); 
+
+    $sqlsrvDB->where('id_cat_profesional', $postData['id_cat_profesional']);      
+    $resultado=$sqlsrvDB->update('cat_profesionales',$data);  		
+    /*************************************************************************/
+    
+    $data = array(  
+    'id_cat_usuario'  => null,             
+    'id_cat_profesional'  => $postData['id_cat_profesional'],          
+    'id_cat_estado'  => $postData['id_cat_estado'], 
+    'municipio'  => $postData['mpio'], 
+    'colonia'  => $postData['colonia'], 
+    'calle'  => $postData['calle'], 
+    'num'  => $postData['num'], 
+    'cp'  => $postData['cp'],         
+    'tel'  => $postData['telefono'],    
+    'dom_particular' => 1,      
+    'activo' => 1
+    );      
+
+    if ($postData['existe_direccion']=="si")
+    {
+      $sqlsrvDB->where('id_cat_profesional', $postData['id_cat_profesional']);      
+      $sqlsrvDB->where('dom_particular', '1');      
+      $resultado=$sqlsrvDB->update('cat_direcciones',$data);    
+    }
+    else 
+      $resultado=$sqlsrvDB->insert('cat_direcciones',$data);           
+      
+      
+      return $resultado;              
+    } 
+
+
+    public function CatalogoEstados()
+    {
+    $sqlsrvDB = $this->load->database('dbProfesiolandia',TRUE);
+    $query="select * from cat_estados where activo=1 order by nombre";         
+        $resultado = $sqlsrvDB->query($query);		
+    return $resultado->result();        
+    } 
+
+	
+
+}
+
+?>
