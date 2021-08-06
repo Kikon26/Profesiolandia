8439103c227c9c652fc23697c5ade9dfb1718072
Martin Alberto Solis Trejo <martin.solis@morelia.gob.mx>
2021-08-06 16:22:28 -0500

    CORREOS Y RESPUESTA A PREGUNTAS

---
+++ application/logs/log-2021-08-06.php
@@ -0,0 +1,310 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-08-06 14:37:24 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 14:37:24 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 14:37:24 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 14:37:24 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 14:51:37 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 14:51:38 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 14:51:38 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 14:51:38 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 14:51:39 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ') AS p LEFT JOIN
+                      (
+                        SELECT 
+    ' at line 10 - Invalid query: SELECT  
+                      p.id_cat_pregunta,
+                      p.pregunta,
+                      r.id_cat_respuesta,
+                      r.respuesta,
+                      r.carrera,
+                      r.profesional, 
+                      r.imagen
+                      FROM 
+                      (SELECT * FROM cat_preguntas WHERE id_cat_profesion= ) AS p LEFT JOIN
+                      (
+                        SELECT 
+                        r.id_cat_pregunta,
+                        r.id_cat_respuesta,
+                        r.respuesta,
+                        pe.nombre AS carrera,
+                        CONCAT(pr.nombre,' ',pr.paterno,' ',pr.materno) AS profesional, 
+                        pr.imagen
+                        FROM                 
+                        cat_respuestas AS r   INNER JOIN 
+                        cat_profesionales AS pr ON pr.id_cat_profesional=r.id_cat_profesional AND pr.id_cat_profesional=28 INNER JOIN 
+                        cat_profesiones AS pe ON pe.id_cat_profesion=pr.id_cat_profesion
+                      ) AS r ON r.id_cat_pregunta=p.id_cat_pregunta
+ERROR - 2021-08-06 14:51:45 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 14:51:45 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 14:51:45 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 14:51:45 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 14:51:46 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ') AS p LEFT JOIN
+                      (
+                        SELECT 
+    ' at line 10 - Invalid query: SELECT  
+                      p.id_cat_pregunta,
+                      p.pregunta,
+                      r.id_cat_respuesta,
+                      r.respuesta,
+                      r.carrera,
+                      r.profesional, 
+                      r.imagen
+                      FROM 
+                      (SELECT * FROM cat_preguntas WHERE id_cat_profesion= ) AS p LEFT JOIN
+                      (
+                        SELECT 
+                        r.id_cat_pregunta,
+                        r.id_cat_respuesta,
+                        r.respuesta,
+                        pe.nombre AS carrera,
+                        CONCAT(pr.nombre,' ',pr.paterno,' ',pr.materno) AS profesional, 
+                        pr.imagen
+                        FROM                 
+                        cat_respuestas AS r   INNER JOIN 
+                        cat_profesionales AS pr ON pr.id_cat_profesional=r.id_cat_profesional AND pr.id_cat_profesional=28 INNER JOIN 
+                        cat_profesiones AS pe ON pe.id_cat_profesion=pr.id_cat_profesion
+                      ) AS r ON r.id_cat_pregunta=p.id_cat_pregunta
+ERROR - 2021-08-06 14:51:46 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 14:51:47 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 14:51:47 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 14:51:47 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 14:51:48 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ') AS p LEFT JOIN
+                      (
+                        SELECT 
+    ' at line 10 - Invalid query: SELECT  
+                      p.id_cat_pregunta,
+                      p.pregunta,
+                      r.id_cat_respuesta,
+                      r.respuesta,
+                      r.carrera,
+                      r.profesional, 
+                      r.imagen
+                      FROM 
+                      (SELECT * FROM cat_preguntas WHERE id_cat_profesion= ) AS p LEFT JOIN
+                      (
+                        SELECT 
+                        r.id_cat_pregunta,
+                        r.id_cat_respuesta,
+                        r.respuesta,
+                        pe.nombre AS carrera,
+                        CONCAT(pr.nombre,' ',pr.paterno,' ',pr.materno) AS profesional, 
+                        pr.imagen
+                        FROM                 
+                        cat_respuestas AS r   INNER JOIN 
+                        cat_profesionales AS pr ON pr.id_cat_profesional=r.id_cat_profesional AND pr.id_cat_profesional=28 INNER JOIN 
+                        cat_profesiones AS pe ON pe.id_cat_profesion=pr.id_cat_profesion
+                      ) AS r ON r.id_cat_pregunta=p.id_cat_pregunta
+ERROR - 2021-08-06 14:51:48 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 14:51:49 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 14:51:49 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 14:51:49 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 14:51:49 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 14:51:50 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 14:51:50 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 14:51:50 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 14:51:51 --> Query error: You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near ') AS p LEFT JOIN
+                      (
+                        SELECT 
+    ' at line 10 - Invalid query: SELECT  
+                      p.id_cat_pregunta,
+                      p.pregunta,
+                      r.id_cat_respuesta,
+                      r.respuesta,
+                      r.carrera,
+                      r.profesional, 
+                      r.imagen
+                      FROM 
+                      (SELECT * FROM cat_preguntas WHERE id_cat_profesion= ) AS p LEFT JOIN
+                      (
+                        SELECT 
+                        r.id_cat_pregunta,
+                        r.id_cat_respuesta,
+                        r.respuesta,
+                        pe.nombre AS carrera,
+                        CONCAT(pr.nombre,' ',pr.paterno,' ',pr.materno) AS profesional, 
+                        pr.imagen
+                        FROM                 
+                        cat_respuestas AS r   INNER JOIN 
+                        cat_profesionales AS pr ON pr.id_cat_profesional=r.id_cat_profesional AND pr.id_cat_profesional=28 INNER JOIN 
+                        cat_profesiones AS pe ON pe.id_cat_profesion=pr.id_cat_profesion
+                      ) AS r ON r.id_cat_pregunta=p.id_cat_pregunta
+ERROR - 2021-08-06 14:52:18 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 14:52:18 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 14:52:18 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 14:52:18 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 14:59:18 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 14:59:19 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 14:59:19 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 14:59:19 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:00:57 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:00:57 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:00:57 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:00:57 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:01:32 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:01:32 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:01:32 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:01:32 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:03:26 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:03:26 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:03:26 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:03:26 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:03:46 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:03:46 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:03:46 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:03:46 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:06:10 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:06:10 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:06:10 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:06:10 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:06:44 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:06:44 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:06:44 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:06:44 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:07:19 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:07:19 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:07:19 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:07:19 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:07:36 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:07:37 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:07:37 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:07:37 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:08:04 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:08:05 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:08:05 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:08:05 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:08:32 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:08:32 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:08:32 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:08:32 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:08:53 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:08:53 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:08:53 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:08:53 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:16:10 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:16:10 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:16:10 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:16:10 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:16:45 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:16:45 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:16:45 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:16:45 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:17:22 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:17:22 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:17:22 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:17:22 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:18:38 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:18:38 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:18:38 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:18:38 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:18:50 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:18:50 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:18:50 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:18:50 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:20:04 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:20:04 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:20:04 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:20:05 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:20:39 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:20:40 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:20:40 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:20:40 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:21:10 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:21:11 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:21:11 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:21:11 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:22:11 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:22:11 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:22:11 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:22:11 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:22:32 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:22:33 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:22:33 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:22:33 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:23:50 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:23:50 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:23:50 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:23:50 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:24:23 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:24:23 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:24:23 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:24:23 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:25:14 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:25:15 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:25:15 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:25:15 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:25:30 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:25:31 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:25:31 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:25:31 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:25:51 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:25:52 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:25:52 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:25:52 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:26:47 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:26:47 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:26:47 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:26:47 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:27:26 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:27:27 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:27:27 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:27:27 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:27:48 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:27:49 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:27:49 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:27:49 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:28:07 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:28:08 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:28:08 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:28:08 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:29:03 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 15:29:03 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 15:29:03 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 15:29:03 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 16:04:13 --> Severity: error --> Exception: syntax error, unexpected ''usuario'' (T_CONSTANT_ENCAPSED_STRING) C:\xampp\htdocs\Profesiolandia\application\controllers\CAltaProfesional.php 389
+ERROR - 2021-08-06 16:04:21 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 16:04:21 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 16:04:21 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 16:04:34 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 16:04:34 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 16:04:34 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 16:05:02 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 16:05:03 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 16:05:03 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 16:05:03 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 16:05:05 --> Severity: error --> Exception: syntax error, unexpected ''usuario'' (T_CONSTANT_ENCAPSED_STRING) C:\xampp\htdocs\Profesiolandia\application\controllers\CAltaProfesional.php 389
+ERROR - 2021-08-06 16:05:49 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 16:05:50 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 16:05:50 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 16:05:50 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 16:08:49 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 16:08:49 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 16:08:49 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 16:08:49 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 16:09:36 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 16:09:37 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 16:09:37 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 16:09:37 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 16:10:33 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 16:10:34 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 16:10:34 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 16:10:34 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 16:10:35 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 16:10:35 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 16:10:35 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 16:10:35 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 16:10:36 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 16:10:36 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 16:10:36 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 16:10:36 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 16:12:09 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 16:12:10 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 16:12:10 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 16:12:10 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 16:13:53 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 16:13:54 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 16:13:54 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 16:13:54 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 16:14:21 --> 404 Page Not Found: Assets/css
+ERROR - 2021-08-06 16:14:22 --> 404 Page Not Found: Assets/js
+ERROR - 2021-08-06 16:14:22 --> 404 Page Not Found: Assets/libs
+ERROR - 2021-08-06 16:14:22 --> 404 Page Not Found: Assets/libs
